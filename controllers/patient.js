5ee9024ef0e0699a03341eeb516ca58f56ca42b3
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2026-05-22 14:48:31 +0200

    practica4-arso

--- controllers/patient.js
+++ controllers/patient.js
@@ -2,45 +2,81 @@
 const Patient = require('../models/patient');
 
 exports.list = async function() {
-    // Rellene aqui ...
-    console.log("entra en el método list")
-    const patients = await Patient.find();
-    console.log("patients: ", patients);
-    return patients;
+    console.log("entra en el método list");
+
+        try{
+            const patients = await Patient.find();
+            console.log("patients: ", patients);
+            return patients;
+        }catch(error){
+            console.error("error de tipo: ",error)
+            throw error;
+        }
 }
 
 exports.read = async function(patientId) {
-    // Rellene aqui ...
+console.log("entra en el método read");
+    try{
+        return await Patient.findById(patientId);
+    }catch(error){
+        throw error;
+    }
+
 }
 
 exports.create = async function(body) {
-    // Rellene aqui ...
+    const newPatient = new Patient(body)
     console.log("BODY: ", body);
-
+    return await newPatient.save();
 }
 
 exports.update= async function(patientId, body) {
-    // Rellene aqui ...
+    let result = await Patient.findOneAndUpdate(
+        { _id: patientId },
+        body,
+        { new: true }
+    );
+
     console.log("BODY: ", body);
     console.log("patientId: ", patientId);
+
+    return result;
+
 }
 
 exports.delete = async function(patientId) {
-    // Rellene aqui ...
+    return await Patient.deleteOne({_id:patientId});
 }
 
 exports.filterPatientsByCity = async function (city) {
-    // Rellene aqui ...
+    return await Patient.find({city : city});
 }
 
 exports.filterPatientsByDiagnosis = async function (diagnosis) {
-    // Rellene aqui ...
-}
+    let result = await Patient.find({
+        "medicalHistory.diagnosis": diagnosis
+    });
+
+    return result;}
 
 exports.filterPatientsBySpeacialistAndDate = async function (specialist, sDate,fDate) {
-    // Rellene aqui ...
+    return await Patient.find({
+        medicalHistory: {
+            $elemMatch: {
+                specialist: specialist,
+                date: {
+                    $gte: new Date(sDate),
+                    $lte: new Date(fDate)
+                }
+            }
+        }
+    });
 }
 
 exports.addPatientHistory = async function (patientId, medicalRecord) {
-    // Rellene aqui ...
+    return await Patient.findOneAndUpdate(
+        { _id: patientId },
+        { $push: { medicalHistory: medicalRecord } },
+        { new: true }
+    );
 }
